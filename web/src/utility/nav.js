b1813c69bedc040165a99f669295c5d4fb8d9d4e
Thiago Senna <trsenna@gmail.com>
2016-12-01 21:54:20 -0200

    cleanup

--- web/src/utility/nav.js
+++ web/src/utility/nav.js
@@ -3,12 +3,7 @@
 define(function (require) {
     var navUtility = {},
         jquery = require('jquery');
-
-    /**
-     * Navigation Item Activate
-     *
-     * @param href
-     */
+    
     navUtility.activate = function (href) {
         navUtility.deactivate();
 
@@ -16,10 +11,6 @@ define(function (require) {
         jquery(query).parent().addClass('active');
     };
 
-    /**
-     * Navigation Item Deactivate
-     *
-     */
     navUtility.deactivate = function () {
         jquery('.nav a').parent().removeClass('active');
     };
